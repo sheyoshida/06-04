424336e852afd3a9f0b72121985ef845c0ada61f
Henna <henna.ahmed92@gmail.com>
2015-06-06 12:10:17 -0400

    test commit for password

--- Variables2/Variables2/main.m
+++ Variables2/Variables2/main.m
@@ -17,7 +17,7 @@ int main(int argc, const char * argv[]) {
                 *border = @"\n+";
         
         //Create top and bottom borders
-        //comment
+        //comment comment
         for (int i = 0; i<50; i++) {
             border = [border stringByAppendingString:@"-"];
         }
